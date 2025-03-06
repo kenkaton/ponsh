ca5b99cb9d507108839461d5a2b8b806eefb5e09
Kenkaton <kkato@nature.global>
2025-03-06 18:33:16 +0900

    rubocop -a

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module Ponsh
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:en, :ja]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.available_locales = [ :en, :ja ]
 
     # Set the default time zone to Tokyo
     config.time_zone = "Tokyo"
