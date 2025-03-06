98c1918e4a60ae5545ba14725809e47145ed71e8
Kenkaton <kkato@nature.global>
2025-03-06 18:31:11 +0900

    set ja location

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,13 @@ module Ponsh
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :ja]
+
+    # Set the default time zone to Tokyo
+    config.time_zone = "Tokyo"
   end
 end
