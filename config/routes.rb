04ea933556af20ea504b747fcf8e31ece40f3c61
Kenkaton <kkato@nature.global>
2025-04-27 15:01:47 +0900

    resources :companies, only: [ :show ] and :brands, only: [ :index, :show ]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :companies
-  resources :brands
+  resources :companies, only: [ :show ]
+  resources :brands, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
