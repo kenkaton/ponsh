7b97a3e57f896c958b40cde339ec8f14c04961ee
Kenkaton <kkato@nature.global>
2025-09-09 17:24:22 +0900

    imple passkey auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern unless Rails.env.development?
+
+  private
+
+  def current_account
+    rodauth.rails_account
+  end
+  helper_method :current_account
+
+  def authenticate
+    rodauth.require_account
+  end
 end
